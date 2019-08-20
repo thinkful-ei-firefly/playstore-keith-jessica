5a19af58e23a82c049eb6d1a0205e4ac3fabac95
Keith Prince <keithcprince@gmail.com>
2019-08-20 11:56:21 -0400

    Sort funtions

--- app.js
+++ app.js
@@ -24,6 +24,26 @@ app.get('/apps', (req, res) => {
     }
     apps = apps.filter(app => app.Genres.toLowerCase().includes(genres.toLowerCase()));
   }
+  if (sort) {
+      if (!(sort.toLowerCase() === "rating") && !(sort.toLowerCase() === "app")){
+        return res
+        .status(400)
+        .send('Must sort by "app" or "rating"');
+      }
+      if (sort.toLowerCase() === 'rating'){
+        apps = apps.sort((app1, app2) => {
+              return app2.Rating - app1.Rating;
+        });
+      }
+      if (sort.toLowerCase() === 'app'){
+          apps = apps.sort((app1, app2) => {
+            if (app2.App.toLowerCase() > app1.App.toLowerCase())
+              return -1;
+            else 
+              return 1;
+          });
+      }
+  }
 
   
 
